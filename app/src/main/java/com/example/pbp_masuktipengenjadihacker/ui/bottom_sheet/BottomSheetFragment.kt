0c6d017416f7945bcc8dc501340aa5e66fa4632d
raflyafrzl <comfortyourstep@gmail.com>
2023-06-25 01:03:22 +0700

    feat:firestorage

--- app/src/main/java/com/example/pbp_masuktipengenjadihacker/ui/bottom_sheet/BottomSheetFragment.kt
+++ app/src/main/java/com/example/pbp_masuktipengenjadihacker/ui/bottom_sheet/BottomSheetFragment.kt
@@ -1,6 +1,11 @@
 package com.example.pbp_masuktipengenjadihacker.ui.bottom_sheet
 
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 
 import android.view.LayoutInflater
 import android.view.View
@@ -13,13 +18,22 @@ import com.example.pbp_masuktipengenjadihacker.databinding.FragmentBottomSheetBi
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
 
 class BottomSheetFragment :BottomSheetDialogFragment() {
+
+
+    private lateinit var  storageRef : StorageReference
+
     private val db = Firebase.firestore
     private lateinit var binding: FragmentBottomSheetBinding
     companion object {
         const val uploadToButton: String = "uploadButtonTag"
+        private const val IMAGE_PICK_REQUEST_CODE =123
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -61,5 +75,40 @@ class BottomSheetFragment :BottomSheetDialogFragment() {
                 Toast.makeText(requireContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show()
             }
         }
+
+        //inisialisasi storage
+        storageRef = FirebaseStorage.getInstance().getReference("uploads")
+
+        //btnUploadGambar
+        binding.uploadConstraint.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, IMAGE_PICK_REQUEST_CODE)
+        }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_PICK_REQUEST_CODE && requestCode == Activity.RESULT_OK){
+            //get img uri
+            val imageUri : Uri? = data?.data
+
+            //upload image to firebase
+            imageUri?.let { uri ->
+                val imageRef = storageRef.child("gs://pbp-masuktipengenjadihacker.appspot.com/${uri.lastPathSegment}")
+                val uploadTask = imageRef.putFile(uri)
+
+                //success atau tidak
+                uploadTask.addOnCompleteListener{ task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(requireContext(),"Success Upload",Toast.LENGTH_SHORT).show()
+                    }else{
+                        val exception = task.exception
+                    }
+                }
+            }
+
+        }
+    }
+
 }
